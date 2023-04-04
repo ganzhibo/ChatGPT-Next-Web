d1cf29ad573b710789c73c39fb087d8e40953f02
ganzhibo <119782251+ganzhibo@users.noreply.github.com>
2023-04-04 13:47:20 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,10 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+// export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const REPO_URL = `https://jq.qq.com/?_wv=1027&k=izNfhkf8`;//这里是首页QQ图标点击的链接
+// export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const ISSUE_URL = `https://ai.chatgpt004.com`;//这里是报错后的地址可以是你官网
+// export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+export const UPDATE_URL = `http://wpa.qq.com/msgrd?v=3&uin=68060250&site=qq&menu=yes`;//这里是更新的地址链接
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
